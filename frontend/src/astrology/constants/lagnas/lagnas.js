7820f146b54814a99810f7b9efe65148d5d45d67
Sreenivasa Chari <japhathakam@gmail.com>
2026-07-09 19:36:28 +0530

    Sprint 6.1B - Add Kala Chakra (Lagna) Master Constants

---
+++ frontend/src/astrology/constants/lagnas/lagnas.js
@@ -0,0 +1,84 @@
+/* ==========================================================
+   JAPHATHAKAM ASTROLOGY ENGINE
+   Kala Chakra (Lagna) Master Data
+-------------------------------------------------------------
+   Purpose : Immutable master data for the 12 Lagnas
+   Version : 1.0
+========================================================== */
+
+const lagnas = Object.freeze([
+    {
+        code: "LAGNA_1",
+        englishName: "Mesha",
+        teluguName: "మేష",
+        order: 1
+    },
+    {
+        code: "LAGNA_2",
+        englishName: "Vrushabha",
+        teluguName: "వృషభ",
+        order: 2
+    },
+    {
+        code: "LAGNA_3",
+        englishName: "Mithuna",
+        teluguName: "మిథున",
+        order: 3
+    },
+    {
+        code: "LAGNA_4",
+        englishName: "Karkataka",
+        teluguName: "కర్కాటక",
+        order: 4
+    },
+    {
+        code: "LAGNA_5",
+        englishName: "Simha",
+        teluguName: "సింహ",
+        order: 5
+    },
+    {
+        code: "LAGNA_6",
+        englishName: "Kanya",
+        teluguName: "కన్య",
+        order: 6
+    },
+    {
+        code: "LAGNA_7",
+        englishName: "Tula",
+        teluguName: "తుల",
+        order: 7
+    },
+    {
+        code: "LAGNA_8",
+        englishName: "Vruschika",
+        teluguName: "వృశ్చిక",
+        order: 8
+    },
+    {
+        code: "LAGNA_9",
+        englishName: "Dhanussu",
+        teluguName: "ధనుస్సు",
+        order: 9
+    },
+    {
+        code: "LAGNA_10",
+        englishName: "Makara",
+        teluguName: "మకర",
+        order: 10
+    },
+    {
+        code: "LAGNA_11",
+        englishName: "Kumbha",
+        teluguName: "కుంభ",
+        order: 11
+    },
+    {
+        code: "LAGNA_12",
+        englishName: "Meena",
+        teluguName: "మీన",
+        order: 12
+    }
+]);
+
+export default lagnas;
