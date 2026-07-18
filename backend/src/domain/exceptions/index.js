beddf1de25e86bd7cf920eae5849486664b8e618
Sreenivasa Chari <japhathakam@gmail.com>
2026-07-18 13:54:08 +0530

    Sprint 8.4.6 - Add Domain exceptions

---
+++ backend/src/domain/exceptions/index.js
@@ -0,0 +1,5 @@
+export { default as DomainException } from './DomainException';
+export { default as InvalidBirthContextException } from './InvalidBirthContextException';
+export { default as InvalidGrahaException } from './InvalidGrahaException';
+export { default as InvalidKundaliException } from './InvalidKundaliException';
+export { default as UnsupportedGrahaException } from './UnsupportedGrahaException';
