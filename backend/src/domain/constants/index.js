ab234eea78b2434939d92f716b86e6d460d15dde
Sreenivasa Chari <japhathakam@gmail.com>
2026-07-18 13:48:32 +0530

    Sprint 8.4.2 - Add Domain contracts

---
+++ backend/src/domain/constants/index.js
@@ -0,0 +1,3 @@
+export { default as IKundaliFactory } from './IKundaliFactory';
+export { default as IKundaliRepository } from './IKundaliRepository';
+export { default as IJaphathakamService } from './IJaphathakamService';
